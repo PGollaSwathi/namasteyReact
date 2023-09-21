20bc4234508c7695a53d81b77c66c2525a1ce2a7
PGollaSwathi <gollaswathi12345@gmail.com>
2023-09-21 18:09:36 +0530

    episode2

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 const heading = React.createElement("h1",{},"hello world")
-const body =ReactDOM.createRoot(document.getElementById("root"))
-body.render(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading);
